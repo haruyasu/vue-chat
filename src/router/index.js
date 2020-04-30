ba01e9c64b116eced954f976352e204a64b8f982
haruyasu <harukun2002@gmail.com>
2020-04-30 16:37:02 +0900

    :rocket: Firebase認証機能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/components/Home";
+import Signup from "@/components/Signup";
+import Login from "@/components/Login";
+import firebase from "firebase";
 
 Vue.use(VueRouter);
 
@@ -8,23 +11,41 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/signup",
+    name: "Signup",
+    component: Signup
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    let user = firebase.auth().currentUser;
+    if (user) {
+      next();
+    } else {
+      next({
+        name: "Login"
+      });
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
